e19b305a1b2d8950623e715e0392f117119ce51d
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2025-10-03 14:22:48 +0100

    Update ratio-calculator.js
     - added ability to remove items when too many were added. also stopped user from deleting items that aren't the most recently added item to help prevent the calculation from not working.

--- ratio-calculator.js
+++ ratio-calculator.js
@@ -13,6 +13,7 @@ addBtn.addEventListener("click", () => {
     let newItem = document.createElement("label");
     let newItemContent = document.createTextNode(`${idCount}:`);
     let newItemVal = document.createElement("input");
+    let btn = document.createElement("button");
       newItem.setAttribute("for", `chem${idCount}`);
       newItem.setAttribute("id", `chem${idCount}`);
       newItemVal.setAttribute("type", "number");
@@ -20,9 +21,24 @@ addBtn.addEventListener("click", () => {
       newItemVal.setAttribute("for", `chem${idCount}`);
       newItemVal.classList.add("chems");
       newItemVal.setAttribute("value", "1");
+      btn.classList.add("discard");
+      btn.setAttribute("type", "button");
+      btn.setAttribute("id", newItem.id);
+      btn.textContent = "X";
       newItem.appendChild(newItemContent);
+      newItem.appendChild(btn);
         document.getElementById("main-list").appendChild(newItem);
-        document.getElementById("main-list").appendChild(newItemVal);
+        document.getElementById("main-list").appendChild(newItemVal);      
+
+        btn.onclick = function removeItem() {
+          console.log(btn.id);
+        let parent = document.getElementById("main-list");
+          if (btn.id === `chem${idCount}`) {
+            parent.removeChild(document.getElementById(newItem.id));
+            parent.removeChild(document.getElementById(newItemVal.id));
+        idCount--;
+          }
+       }
     } else {
       return;
     }
@@ -32,7 +48,6 @@ addBtn.addEventListener("click", () => {
 checkBtn.addEventListener("click", () => {
 
   const sum = [...document.getElementsByClassName("chems")].map((i) => Number(i.value)); //gets the value of each .chems class and puts them into an array
-  
   for (let i = 0; i < sum.length; i++) {
     
   const lowestVal = Math.min(...sum); //finds lowest val that isnt null
@@ -40,7 +55,6 @@ checkBtn.addEventListener("click", () => {
   const ratiosArr = []; //array used for storing ratio values
   const newValArr = [];
   
- //console.log(sum, `lowest val: ${lowestVal}`, sum.reduce((acc, el) => acc + el, 0));
   let formulaTotal = sum.reduce((acc, el) => acc + el, 0) + Number(document.getElementById("diluent-num").value); 
     //adds the values in sum array and diluent number to (hopefully) equal 100.
 
@@ -78,15 +92,14 @@ checkBtn.addEventListener("click", () => {
     
     ratiosArr.push(aVal, bVal, cVal, dVal, eVal, fVal, gVal, hVal, iVal, jVal, kVal, lVal, mVal, nVal, oVal, pVal, qVal, rVal, sVal, tVal, uVal, vVal, wVal, xVal, yVal, zVal, aaVal, abVal, acVal, adVal);   //pushes all "x"Val values into an array so i can sum up their total ratio values
     const newArr = ratiosArr.filter(value => !Number.isNaN(value) );  //removes all 'null' values from array to stop it breaking
-
   let newRatioTotal = newArr.reduce((acc, el) => acc + el, 0);  //total value of all ratios added together
-  //console.log(`ratios: ${ratiosArr}`, `ratios total: ${newRatioTotal}`, ratiosArr, newArr);
     //difference between curr and new diluent
   let diluentDiff = document.getElementById("diluent-num").value - document.getElementById("diluent-new-num").value;
     //remaining amount afer new diluent taken from total (or 100)
   let remainderVal = 100 - document.getElementById("diluent-new-num").value;
-  let newMulti = remainderVal / newRatioTotal;  //remainder divided by total ratio gives me the new 'lowestVal', i can now multiply each of the 'x'Val values by this to give me their new value that fits in with the new diluent to create an accurate formula that totals 100
-
+  let r = (remainderVal / newRatioTotal).toFixed(3); 
+    //remainder divided by total ratio gives me the new 'lowestVal', i can now multiply each of the 'x'Val values by this to give me their new value that fits in with the new diluent to create an accurate formula that totals 100
+  let newMulti = parseFloat(r);
     //using to.Fixed so recurring numbers get rounded to make readability easier
     let aValNew = Number((aVal * newMulti).toFixed(3));
     let bValNew = Number((bVal * newMulti).toFixed(3));
@@ -122,46 +135,17 @@ checkBtn.addEventListener("click", () => {
     
     //newVals are pushed into array, including nulls which get taen care of below by being filtered out like before
     newValArr.push(aValNew, bValNew, cValNew, dValNew, eValNew, fValNew, gValNew, hValNew, iValNew, jValNew, kValNew, lValNew, mValNew, nValNew, oValNew, pValNew, qValNew, rValNew, sValNew, tValNew, uValNew, vValNew, wValNew, xValNew, yValNew, zValNew, aaValNew, abValNew, acValNew, adValNew);
-    const newValArr2 = newValArr.filter(value => !Number.isNaN(value) );
-    
+    const newValArr2 = newValArr.filter(value => !Number.isNaN(value));
       let newValArrTotal = newValArr2.reduce((acc, el) => acc + el, 0); //totalling the new values
         //adding the new values to the new diluent value to return a complete (100) formulation
       let newTotal = (newValArrTotal + Number(document.getElementById("diluent-new-num").value)).toFixed(0);
-      //console.log(diluentDiff, remainderVal, newMulti, newTotal);
+      console.log(diluentDiff, remainderVal, newMulti, newTotal);
        
     document.getElementById("diluent-num").value = document.getElementById("diluent-new-num").value;  //guaranteed to be displayed
     document.getElementById("1").value = aValNew; //guaranteed to be displayed
-    document.getElementById("2") ? document.getElementById("2").value = bValNew : ""; //conditional ternary to check if value is present, if so amend the HTML
-    document.getElementById("3") ? document.getElementById("3").value = cValNew : "";
-    document.getElementById("4") ? document.getElementById("4").value = dValNew : "";
-    document.getElementById("5") ? document.getElementById("5").value = eValNew : "";
-    document.getElementById("6") ? document.getElementById("6").value = fValNew : "";
-    document.getElementById("7") ? document.getElementById("7").value = gValNew : "";
-    document.getElementById("8") ? document.getElementById("8").value = hValNew : "";
-    document.getElementById("9") ? document.getElementById("9").value = iValNew : "";
-    document.getElementById("10") ? document.getElementById("10").value = jValNew : "";
-    document.getElementById("11") ? document.getElementById("11").value = kValNew : "";
-    document.getElementById("12") ? document.getElementById("12").value = lValNew : "";
-    document.getElementById("13") ? document.getElementById("13").value = mValNew : "";
-    document.getElementById("14") ? document.getElementById("14").value = nValNew : "";
-    document.getElementById("15") ? document.getElementById("15").value = oValNew : "";
-    document.getElementById("16") ? document.getElementById("16").value = pValNew : "";
-    document.getElementById("17") ? document.getElementById("17").value = qValNew : "";
-    document.getElementById("18") ? document.getElementById("18").value = rValNew : "";
-    document.getElementById("19") ? document.getElementById("19").value = sValNew : "";
-    document.getElementById("20") ? document.getElementById("20").value = tValNew : "";
-    document.getElementById("21") ? document.getElementById("21").value = uValNew : "";
-    document.getElementById("22") ? document.getElementById("22").value = vValNew : "";
-    document.getElementById("23") ? document.getElementById("23").value = wValNew : "";
-    document.getElementById("24") ? document.getElementById("24").value = xValNew : "";
-    document.getElementById("25") ? document.getElementById("25").value = yValNew : "";
-    document.getElementById("26") ? document.getElementById("26").value = zValNew : "";
-    document.getElementById("27") ? document.getElementById("27").value = aaValNew : "";
-    document.getElementById("28") ? document.getElementById("28").value = abValNew : "";
-    document.getElementById("29") ? document.getElementById("29").value = acValNew : "";
-    document.getElementById("30") ? document.getElementById("30").value = adValNew : "";
-    //capped at 30 but can be expanded if needed.
-    
+    for (let i = 2; i <= 30; i++) {
+      document.getElementById(`${i}`) ? document.getElementById(`${i}`).value = newValArr2[i-1] : "";
+    }
     document.getElementById("formula-total").innerText = "Formula total: " + newTotal;  //formula total updated for completeness
   }
 });
